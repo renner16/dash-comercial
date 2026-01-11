b908b8bf5cdd1305765aa60b3ddac233417388b8
Renner Araújo <renner.1607@hotmail.com>
2026-01-11 16:47:23 -0300

    fix: remove props spread from Chevron icon to prevent runtime error

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -62,9 +62,9 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        Chevron: ({ orientation, ...props }) => {
+        Chevron: ({ orientation }) => {
           const Icon = orientation === "left" ? ChevronLeft : ChevronRight
-          return <Icon className="h-4 w-4" {...props} />
+          return <Icon className="h-4 w-4" />
         },
       }}
       {...props}
